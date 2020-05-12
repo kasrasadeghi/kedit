ef9f6385183bf6d0862c67f5c76a0f49882e2a83
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-05-12 12:55:26 -0500

    delete unneeded cmake packages

--- cmake/libnoise.cmake
+++
@@ -1,7 +0,0 @@
-# find_package(libnoise REQUIRED)
-set(LIBNOISE_LIBRARY noise)
-# set(LIBNOISE_INCLUDE_DIR /usr/include)
-# message(STATUS "libnoise found with lib:'${LIBNOISE_LIBRARY}' and include:'${LIBNOISE_INCLUDE_DIR}'")
-
-LIST(APPEND stdgl_libraries noise)
-# INCLUDE_DIRECTORIES(${})
