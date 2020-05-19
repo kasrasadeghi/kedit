eb87a47197bb540929d3cde6a3106da9e0b15b19
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-05-19 12:03:37 -0500

    fixed glad includes and add debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Editor.hpp"
+#include "DebugCallback.hpp"
 
 #include <kgfx/RenderWindow.hpp>
 #include <kgfx/TextRenderer.hpp>
@@ -60,8 +61,8 @@ int main() {
   // glEnable(GL_DEPTH_TEST);
   // glDepthFunc(GL_LESS);
 
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_BACK);
+  // glEnable(GL_CULL_FACE);
+  // glCullFace(GL_BACK);
 
   // glEnable(GL_BLEND);
   // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
