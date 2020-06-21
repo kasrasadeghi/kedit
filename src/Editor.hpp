66732cc7834e1be91fa735c9bcb3dd516180584b
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-06-21 07:05:57 -0500

    finish pointer-move by freeing both filebuffers and menus
    - add "W" shortcut to close current page

--- src/Editor.hpp
+++ src/Editor.hpp
@@ -191,36 +191,180 @@ struct Editor {
       return &curr;
     }
 
+  // Commentary:
+  //      A menu exists in 3 different contexts:
+  //   1. _buffers, where its buffer lives
+  //   2. _menus,   where the Menu itself and the Page lives as a component.
+  //                The Page points to the buffer.
+  //   3. _pages,   where a pointer to its Page lives
+  //      To free a menu, we must free its buffer, which may move the buffers, which
+  // are refered to by the Pages in both _menus and _filebuffers.  We can use _pages
+  // to access all Pages, and thus fix all buffers pointers which are equally
+  // affected.  We can remove the pointer in _pages for the menu that's being freed.
+  // Because neither _menus nor _buffers refers to _pages, and we only use _pages
+  // to fix buffer pointers, we can free the Page* in _pages first, as we don't need
+  // to fix the Page* for the Menu we are about to remove anyways.
+  // So far:
+  //   1. free Page* in _pages.
+  //   2. free Buffer in _buffers.  <- Keep track of the buffers delta,
+  //                                   fix the pages that remain.
+  //   3. free Menu in _menus
   inline void freeMenu(Menu* menu)
     {
-      // TODO need to fix pointers in here too
-      return;
+      // garbage collect from _pages
+      _pages.erase(std::remove_if(_pages.begin(), _pages.end(),
+                                  [&](Page* curr_page) { return curr_page == (Page*)menu; }),
+                   _pages.end());
+      // NOTE: nobody refers to pages, so there's no pointer-moving to do
 
       // buffer* is in page, in menu
       auto* buffptr = menu->page.buffer;
+      auto* buffer_before = _buffers.data();
       // TODO optimize. can just memcpy instead of erase-remove idiom
-      _buffers.erase(std::remove_if(_buffers.begin(), _buffers.end(), [&](const Buffer& buf) { return buffptr == &buf; }), _buffers.end());
+      _buffers.erase(std::remove_if(_buffers.begin(), _buffers.end(),
+                                    [&](const Buffer& buf) { return &buf == buffptr; }),
+                     _buffers.end());
+      auto* buffer_after = _buffers.data();
 
-      // garbage collect from _pages
-      if (_pages.back() != (Page*)menu)
+      // pointer-move _pages to handle _buffer move
+      if (buffer_before != buffer_after)
         {
-          printerrln("LEAK: cannot garbage collect current menu from _pages");
-          return;
+          println("LOG: _buffers shrink with move");
+          for (auto* page : _pages)
+            {
+              auto old_index = (page->buffer - buffer_before);
+              page->buffer = buffer_after + old_index;
+            }
         }
-      _pages.pop_back();
+      else
+        {
+          println("LOG: _buffers shrink without move");
+        }
 
       // garbage collect from _menus
-      size_t menu_index = menu - _menus.data();
-      if (menu_index != _menus.size() - 1)
+      // - pointer-move _pages if _menus moves
+      std::vector<unsigned char> pages_filter;
+      for (auto* page : _pages)
         {
-          printerrln("LEAK: cannot garbage collect current menu from _menus");
-          return;
+          pages_filter.push_back(page->_type == Type::MenuT);
         }
-      _menus.pop_back();
+
+      auto* menu_before = _menus.data();
+      _menus.erase(std::remove_if(_menus.begin(), _menus.end(),
+                                  [&](const Menu& curr_menu) { return &curr_menu == menu; }),
+                   _menus.end());
+      auto* menu_after = _menus.data();
+
+      // TODO pointer move pages
+      if (menu_before != menu_after)
+        {
+          println("LOG: _menus shrink with move");
+
+          // zip(pages_filter, _pages)
+          for (size_t i = 0; i < pages_filter.size(); ++ i)
+            {
+              if (pages_filter[i])
+                {
+                  auto old_index = (((Menu*)_pages[i]) - menu_before);
+                  _pages[i] = (Page*)(menu_after + old_index);
+                }
+            }
+        }
+      else
+        {
+          println("LOG: _menus grow without move");
+        }
     }
 
+  inline void freeFileBuffer(FileBuffer* filebuffer)
+    {
+      // garbage collect from _pages
+      _pages.erase(std::remove_if(_pages.begin(), _pages.end(),
+                                  [&](Page* curr_page) { return curr_page == (Page*)filebuffer; }),
+                   _pages.end());
+      // NOTE: nobody refers to pages, so there's no pointer-moving to do
+
+      // buffer* is in page, in filebuffer
+      auto* buffptr = filebuffer->page.buffer;
+      auto* buffer_before = _buffers.data();
+      // TODO optimize. can just memcpy instead of erase-remove idiom
+      _buffers.erase(std::remove_if(_buffers.begin(), _buffers.end(),
+                                    [&](const Buffer& buf) { return &buf == buffptr; }),
+                     _buffers.end());
+      auto* buffer_after = _buffers.data();
+
+      // pointer-move _pages to handle _buffer move
+      if (buffer_before != buffer_after)
+        {
+          println("LOG: _buffers shrink with move");
+          for (auto* page : _pages)
+            {
+              auto old_index = (page->buffer - buffer_before);
+              page->buffer = buffer_after + old_index;
+            }
+        }
+      else
+        {
+          println("LOG: _buffers shrink without move");
+        }
+
+      // garbage collect from _filebuffers
+      // - pointer-move _pages if _filebuffers moves
+      std::vector<unsigned char> pages_filter;
+      for (auto* page : _pages)
+        {
+          pages_filter.push_back(page->_type == Type::FileBufferT);
+        }
+
+      auto* filebuffer_before = _filebuffers.data();
+      _filebuffers.erase(std::remove_if(_filebuffers.begin(), _filebuffers.end(),
+                                        [&](const FileBuffer& curr_filebuffer) { return &curr_filebuffer == filebuffer; }),
+                   _filebuffers.end());
+      auto* filebuffer_after = _filebuffers.data();
+
+      // TODO pointer move pages
+      if (filebuffer_before != filebuffer_after)
+        {
+          println("LOG: _filebuffers shrink with move");
+
+          // zip(pages_filter, _pages)
+          for (size_t i = 0; i < pages_filter.size(); ++ i)
+            {
+              if (pages_filter[i])
+                {
+                  auto old_index = (((FileBuffer*)_pages[i]) - filebuffer_before);
+                  _pages[i] = (Page*)(filebuffer_after + old_index);
+                }
+            }
+        }
+      else
+        {
+          println("LOG: _filebuffers grow without move");
+        }
+    }
+
+  // TODO could have optimization for current menu, _pages.pop_back instead of erase-remove
   inline void freeCurrentMenu(void)
     { freeMenu(currentMenu()); }
+
+  inline void freeCurrentFileBuffer(void)
+    { freeFileBuffer(currentFileBuffer()); }
+
+  inline void freeCurrent(void)
+    {
+      if (Type::FileBufferT == currentPage()->_type)
+        {
+          freeCurrentFileBuffer();
+        }
+      else if (Type::MenuT == currentPage()->_type)
+        {
+          freeCurrentMenu();
+        }
+      else
+        {
+          printerrln("ERROR: freeCurrent() with current page type: ", currentPage()->_type);
+        }
+    }
 
   inline Page* currentPage(void)
     { return _pages.back(); }
@@ -368,6 +512,10 @@ struct Editor {
         {
           if (GLFW_KEY_B == key)
             openBrowser();
+
+          if (GLFW_KEY_W == key)
+            if (_pages.size() > 1)
+              freeCurrent();
         }
 
       // TODO closing buffer, switching active buffer
