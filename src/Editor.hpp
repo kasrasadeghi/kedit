86f016b3d579bed78ea5a24a8f029ad4a6844eb1
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-08-07 00:10:23 -0400

    fix menu cursor
    - cursor for menus is not cursor element,
       it's the line of the current selection
    - "cursor" represents the index of the current selection

--- src/Editor.hpp
+++ src/Editor.hpp
@@ -257,7 +257,13 @@ struct Editor {
     {
       switch (currentPage()->_type) {
       case Type::FileBufferT: return currentFileBuffer()->cursor;
-      case Type::MenuT: return Cursor{currentMenu()->cursor, 0};
+      case Type::MenuT:
+        if (currentMenu()->selectable_lines.empty())
+          {
+            printerrln("WARNING: Cannot get cursor of menu with no selectable lines");
+            return Cursor{0, 0};
+          }
+        return Cursor{currentMenu()->selectable_lines[currentMenu()->cursor], 0};
       default:
         printerrln("WARNING: Page of type ", currentPage()->_type, " encountered.");
         return Cursor{0, 0};
@@ -271,6 +277,8 @@ struct Editor {
         {
           currentFileBuffer()->addCursor(gc, _control_mode);
         }
+      // TODO: fix mouse scrolling
+      // - should only scroll to cursor when cursor is interacted with (arrow keys)
       currentPage()->scrollToCursor(gc, getCurrentCursor());
       currentPage()->highlightLine(gc, getCurrentCursor());
     }
