076f85f3d16f42a2def5f729dc4ea98a9119d0f5
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-09-07 09:53:37 -0400

    cleanup render in Menu and FileBuffer

--- src/Menu.hpp
+++ src/Menu.hpp
@@ -88,22 +88,20 @@ struct Menu {
   // - add bold to rope sections
   inline void render(GraphicsContext& gc)
     {
-      uint64_t line_number = 0;
-
       double xpos = page.top_left_position.x + page.offset.x;
       double ypos = page.top_left_position.y + page.offset.y;
 
-      for (size_t i = 0; i < page.buffer.contents.lines.size(); ++i)
+      for (size_t line_i = 0; line_i < page.buffer.contents.lines.size(); ++line_i)
         {
-          StringView line = page.buffer.contents.lines[i];
+          StringView line = page.buffer.contents.lines.at(line_i);
 
           // TODO make text render clip off viewable area
           double current_offset = (gc.line_height * page.buffer.line_scroller.position);
-          double curr_ypos = current_offset + (ypos + (gc.line_height * line_number));
+          double curr_ypos = current_offset + (ypos + (gc.line_height * line_i));
 
           if (curr_ypos < (uint64_t)(gc.window->height() + gc.line_height) && curr_ypos > (uint64_t)(0))
             {
-              if (not selectable_lines.empty() && i == this->selectable_lines[this->cursor])
+              if (not selectable_lines.empty() && line_i == this->selectable_lines[this->cursor])
                 {
                   gc.text(line, xpos, curr_ypos, glm::vec4(1, 1, 1, 1));
                 }
@@ -112,7 +110,6 @@ struct Menu {
                   gc.text(line, xpos, curr_ypos, glm::vec4(0.7, 0.7, 0.7, 1));
                 }
             }
-          ++ line_number;
         }
     }
 
