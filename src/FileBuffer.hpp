a2dc563e874cd3d4de6ee7056beb568f22bb25c9
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-09-07 09:53:37 -0400

    cleanup render in Menu and FileBuffer

--- src/FileBuffer.hpp
+++ src/FileBuffer.hpp
@@ -89,25 +89,24 @@ struct FileBuffer {
 
   inline void render(GraphicsContext& gc)
     {
-      uint64_t line_number = 0;
-
       double xpos = page.top_left_position.x + page.offset.x;
       double ypos = page.top_left_position.y + page.offset.y;
 
       for (size_t line_i = 0; line_i < page.buffer.contents.lines.size(); ++line_i)
         {
-          StringView line = page.buffer.contents.lines[line_i];
-          double current_offset = (gc.line_height * page.buffer.line_scroller.position);
-          double curr_ypos = current_offset + (ypos + (gc.line_height * line_number));
+          StringView line = page.buffer.contents.lines.at(line_i);
 
-          if (curr_ypos < (uint64_t)(gc.window->height() + gc.line_height) && curr_ypos > (uint64_t)(0))
+          double current_offset = (gc.line_height * page.buffer.line_scroller.position);
+          double curr_ypos = current_offset + (ypos + (gc.line_height * line_i));
+
+          if (curr_ypos < (uint64_t)(gc.window->height() + gc.line_height)
+           && curr_ypos > (uint64_t)(0))
             {
               if (cursor.line == line_i)
                 gc.text(line, xpos, curr_ypos, glm::vec4{1});
               else
                 gc.text(line, xpos, curr_ypos, glm::vec4{0.8, 0.8, 0.8, 1});
             }
-          ++ line_number;
         }
     }
 
