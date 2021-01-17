fca319b4e168f8d004a8eb3a6792e9f0e770b38d
kasrasadeghi <kasra.sadeghi1@gmail.com>
2021-01-17 17:57:12 -0500

    FileBuffer: extract render to FileBufferRender

--- src/FileBuffer.hpp
+++ src/FileBuffer.hpp
@@ -96,63 +96,11 @@ struct FileBuffer {
         && (uint64_t)(file.size()) > page.buffer.contents.length();
     }
 
-  inline void render(GraphicsContext& gc)
-    {
-      double xpos = page.top_left_position.x + page.offset.x;
-      double ypos = page.top_left_position.y + page.offset.y;
+  /// Render ===------------------------------------------------------------------------===///
 
-      for (size_t line_i = 0; line_i < page.buffer.contents.lines.size(); ++line_i)
-        {
-          StringView line = page.buffer.contents.lines.at(line_i);
-
-          double current_offset = (gc.line_height * page.buffer.line_scroller.position);
-          double curr_ypos = current_offset + (ypos + (gc.line_height * line_i));
-
-          if (curr_ypos < (uint64_t)(gc.window->height() + gc.line_height)
-           && curr_ypos > (uint64_t)(0))
-            {
-              if (cursor.line == line_i)
-                gc.text(line, xpos, curr_ypos, glm::vec4{1});
-              else
-                gc.text(line, xpos, curr_ypos, glm::vec4{0.8, 0.8, 0.8, 1});
-            }
-        }
-    }
-
-  inline void addCursors(GraphicsContext& gc, bool control_mode)
-    {
-      // TODO change for variable text width fonts
-      float text_width = gc.tr.textWidth("a");
-
-      gc.drawRectangle(page.textCoord(gc, cursor), {text_width, gc.line_height},
-                       control_mode
-                       ? glm::vec4{1, 0.7, 0.7, 0.5}
-                       : glm::vec4{0.7, 1, 0.7, 0.5}, 0.4);
-
-      gc.drawRectangle(page.textCoord(gc, shadow_cursor),
-                       {text_width, gc.line_height},
-                       {0.7, 0.7, 1.0, 0.5}, 0.4);
-
-      // TODO investigate positive z layer being below text?
-      // - maybe ortho proj doesn't flip?
-    }
-
-  inline void addSearchResults(GraphicsContext& gc)
-    {
-      // TODO change for variable text width fonts
-      float text_width = gc.tr.textWidth("a");
-
-      for (Cursor result : _search.results)
-        {
-          // SOON this needs to change to update search queries updating better
-          gc.drawRectangle(page.textCoord(gc, result),
-                           {text_width * _search.common->_query.length(), gc.line_height},
-                           glm::vec4{1, 1, 0.3, 0.4}, 0.3);
-        }
-
-      // TODO investigate positive z layer being below text?
-      // - maybe ortho proj doesn't flip?
-    }
+  void render(GraphicsContext& gc);
+  void addCursors(GraphicsContext& gc, bool control_mode);
+  void addSearchResults(GraphicsContext& gc);
 
   /// Interaction ===-------------------------------------------------------------------===///
 
---
+++ src/FileBufferRender.cpp
@@ -0,0 +1,59 @@
+#include "FileBuffer.hpp"
+
+void FileBuffer::render(GraphicsContext& gc)
+  {
+    double xpos = page.top_left_position.x + page.offset.x;
+    double ypos = page.top_left_position.y + page.offset.y;
+
+    for (size_t line_i = 0; line_i < page.buffer.contents.lines.size(); ++line_i)
+      {
+        StringView line = page.buffer.contents.lines.at(line_i);
+
+        double current_offset = (gc.line_height * page.buffer.line_scroller.position);
+        double curr_ypos = current_offset + (ypos + (gc.line_height * line_i));
+
+        if (curr_ypos < (uint64_t)(gc.window->height() + gc.line_height)
+         && curr_ypos > (uint64_t)(0))
+          {
+            if (cursor.line == line_i)
+              gc.text(line, xpos, curr_ypos, glm::vec4{1});
+            else
+              gc.text(line, xpos, curr_ypos, glm::vec4{0.8, 0.8, 0.8, 1});
+          }
+      }
+  }
+
+void FileBuffer::addCursors(GraphicsContext& gc, bool control_mode)
+  {
+    // TODO change for variable text width fonts
+    float text_width = gc.tr.textWidth("a");
+
+    gc.drawRectangle(page.textCoord(gc, cursor), {text_width, gc.line_height},
+                     control_mode
+                     ? glm::vec4{1, 0.7, 0.7, 0.5}
+                     : glm::vec4{0.7, 1, 0.7, 0.5}, 0.4);
+
+    gc.drawRectangle(page.textCoord(gc, shadow_cursor),
+                     {text_width, gc.line_height},
+                     {0.7, 0.7, 1.0, 0.5}, 0.4);
+
+    // TODO investigate positive z layer being below text?
+    // - maybe ortho proj doesn't flip?
+  }
+
+void FileBuffer::addSearchResults(GraphicsContext& gc)
+  {
+    // TODO change for variable text width fonts
+    float text_width = gc.tr.textWidth("a");
+
+    for (Cursor result : _search.results)
+      {
+        // SOON this needs to change to update search queries updating better
+        gc.drawRectangle(page.textCoord(gc, result),
+                         {text_width * _search.common->_query.length(), gc.line_height},
+                         glm::vec4{1, 1, 0.3, 0.4}, 0.3);
+      }
+
+    // TODO investigate positive z layer being below text?
+    // - maybe ortho proj doesn't flip?
+  }
